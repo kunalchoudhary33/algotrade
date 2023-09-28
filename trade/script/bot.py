6ba8851f19a3c8602d28238b0f7edebc205af281
kunalchoudhary33 <39556304+kunalchoudhary33@users.noreply.github.com>
2023-09-28 15:42:29 +0000

    New changes

--- trade/script/bot.py
+++ trade/script/bot.py
@@ -30,15 +30,17 @@ class Bot():
         self.kite.set_access_token(access_token=self.access_token)
 
 
-    def select_strike_price(self, value1, value2, value3, value4):
+    def select_strike_price(self, value1, value2, value3, value4, value5):
         value1_price = self.kite.quote('NFO:'+str(value1))['NFO:'+str(value1)]["last_price"]
         value2_price = self.kite.quote('NFO:'+str(value2))['NFO:'+str(value2)]["last_price"]
         value3_price = self.kite.quote('NFO:'+str(value3))['NFO:'+str(value3)]["last_price"]
         value4_price = self.kite.quote('NFO:'+str(value4))['NFO:'+str(value4)]["last_price"]
+        value5_price = self.kite.quote('NFO:'+str(value5))['NFO:'+str(value5)]["last_price"]
         diff1 = abs(value1_price - 100)
         diff2 = abs(value2_price - 100)
         diff3 = abs(value3_price - 100)
         diff4 = abs(value4_price - 100)
+        diff5 = abs(value5_price - 100)
         min_diff = min(diff1, diff2, diff3, diff4)
         if min_diff == diff1:
             return value1
@@ -46,22 +48,34 @@ class Bot():
             return value2
         elif min_diff == diff3:
             return value3
-        else:
+        elif min_diff == diff4:
             return value4
+        else:
+            return value5
 
     def strike_price(self):
         ticks = self.kite.quote('NSE:NIFTY 50')
         ltp = ticks['NSE:NIFTY 50']['last_price']
+        
         atm = round(ltp / 50) * 50
-        otm = atm - 50
-        itm = atm + 50
-        itm1 = atm + 50 + 50
+        if(self.option[0]=="PE"):
+            otm = atm - 50
+            itm = atm + 50
+            itm1 = atm + 50 + 50
+            itm2 = atm + 50 + 50 + 50
+        elif(self.option[0]=="CE"):
+            otm = atm + 50
+            itm = atm - 50
+            itm1 = atm - 50 - 50
+            itm2 = atm - 50 - 50 - 50
+
         atm_symbol = "NIFTY23"+str(self.expiry[0])+str(atm)+self.option[0]
         otm_symbol = "NIFTY23"+str(self.expiry[0])+str(otm)+self.option[0]
         itm_symbol = "NIFTY23"+str(self.expiry[0])+str(itm)+self.option[0]
         itm1_symbol = "NIFTY23"+str(self.expiry[0])+str(itm1)+self.option[0]
+        itm2_symbol = "NIFTY23"+str(self.expiry[0])+str(itm2)+self.option[0]
 
-        strike_price = self.select_strike_price(atm_symbol, otm_symbol, itm_symbol, itm1_symbol)
+        strike_price = self.select_strike_price(atm_symbol, otm_symbol, itm_symbol, itm1_symbol, itm2_symbol)
         return strike_price
     
 
@@ -95,64 +109,41 @@ class Bot():
     
 
     def auto_trade(self):
-        stoploss_per = 0.89
-        target_per = 1.10
-        train_sl_per = 0.95
+
         tradingsymbol, quantity, buy_price = self.get_position()
+        if(buy_price < 100):
+            target = round(buy_price + 10)
+            stoploss = round(buy_price - 10)
+        else:
+            target = round(buy_price * 1.10)
+            stoploss = round(buy_price * 0.90)
+
         logging.info("tradingsymbol : "+str(tradingsymbol))
         logging.info("quantity : "+str(quantity))
         logging.info("buy_price : "+str(buy_price))
-
-        stoploss = round(stoploss_per * buy_price)
-        target = round(target_per * buy_price)
         logging.info("stoploss : "+str(stoploss))
         logging.info("target : "+str(target))
 
         full_qty = int(self.quantity[0])
-        half_qty = int(self.quantity[0] / 2)
 
-        trailsl = False
-        max_point = 0
         while True:
             ltp = self.kite.quote('NFO:'+str(tradingsymbol))['NFO:'+str(tradingsymbol)]["last_price"] 
 
-            if(trailsl == False):
-                if(ltp <= stoploss):
-                    order_id = self.kite.place_order('regular', 'NFO', tradingsymbol, 'SELL', full_qty, 'NRML', 'MARKET')
-                    logging.info("Stop loss hit")
-                    break
-                elif(ltp >= target):
-                    order_id = self.kite.place_order('regular', 'NFO', tradingsymbol, 'SELL', half_qty, 'NRML', 'MARKET')
-                    profit = (target - stoploss) * half_qty
-                    logging.info("Target Hit. Profit booked for 1st milestone. "+str(profit))
+            if(ltp <= stoploss):
+                order_id = self.kite.place_order('regular', 'NFO', tradingsymbol, 'SELL', full_qty, 'NRML', 'MARKET')
+                logging.info("Stop loss hit")
+                break
+            elif(ltp >= target):
+                order_id = self.kite.place_order('regular', 'NFO', tradingsymbol, 'SELL', full_qty, 'NRML', 'MARKET')
+                profit = (target - buy_price) * full_qty
+                logging.info("Target Hit. Profit booked for 1st milestone. "+str(profit))
+                break
+        
 
-                    trailsl = True
-                    max_point = ltp
-                    stoploss = buy_price
-
-            elif(trailsl == True):
-                if(ltp >= max_point):
-                    max_point = ltp
-                    stoploss = (train_sl_per * max_point)
-                    min_profit = (stoploss - buy_price) * half_qty
-                    logging.info("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-                    logging.info("New Stoploss : "+str(stoploss))
-                    logging.info("max_point : "+str(max_point))
-                    logging.info("min_profit : "+str(min_profit))
-
-                if(ltp <= stoploss):
-                    order_id = self.kite.place_order('regular', 'NFO', tradingsymbol, 'SELL', half_qty, 'NRML', 'MARKET')
-                    logging.info("Trailing SL Hit")
-                    break
-
-            time.sleep(0.25)       
+            time.sleep(0.20)       
     
-
 
 
 cls = Bot()
 cls.place_buy_order()
 cls.auto_trade()
-
-
-
